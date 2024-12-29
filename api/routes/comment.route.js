abb08dbedfac233aa07f2f7c42b9cf6641822651
I-am-nagesh <i.am.nageshpandey@gmail.com>
2024-12-29 06:49:08 +0530

    added recent articles section

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,10 +1,14 @@
 import express from "express";
-const router = express.Router();
-import { cretateComment,  getPostComments } from "../controllers/comment.controller.js";
+
+import {
+  createComment,
+  getPostComments,
+} from "../controllers/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
-router.post("/create", verifyToken, cretateComment);
-router.get('/getPostComments/:postId', getPostComments);
+const router = express.Router();
 
+router.post("/create", verifyToken, createComment);
+router.get("/getPostComments/:postId", getPostComments);
 
 export default router;
